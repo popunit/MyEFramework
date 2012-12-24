29ec2206a9011efc91cc2b5ee983e4de31ddd6bf
Patrick Yu <pluy_yu@hotmail.com>
2012-12-24 22:56:09 +0800

    Added AnnouncementEndpoints to send online / offline notifications

---
+++ eCommerce.Wcf.Framework/Discovery/DiscoveryProxyService.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace eCommerce.Wcf.Framework.Discovery
+{
+    /// <summary>
+    /// Use a single discovery proxy service to send and receive announcements
+    /// </summary>
+    /// <remarks>http://msdn.microsoft.com/en-us/library/dd456787.aspx</remarks>
+    /*
+     * We can use AnnouncementService to achieve this too.
+     * e.g.
+     *      AnnouncementService announcementService = new AnnouncementService();
+            announcementService.OnlineAnnouncementReceived += (sender, e) =>
+            {
+                string contractTypes = string.Empty;
+                Console.WriteLine("Receive Service Online Announcement.");
+                Console.WriteLine("\tAddress: {0}", e.EndpointDiscoveryMetadata.Address.Uri);
+                Console.WriteLine("\tContract: {0}", e.EndpointDiscoveryMetadata.ContractTypeNames[0]);
+            };
+            announcementService.OfflineAnnouncementReceived += (sender, e) =>
+            {
+                string contractTypes = string.Empty;
+                Console.WriteLine("Receive Service Offline Announcement.");
+                Console.WriteLine("\tAddress: {0}", e.EndpointDiscoveryMetadata.Address.Uri);
+                Console.WriteLine("\tContract: {0}", e.EndpointDiscoveryMetadata.ContractTypeNames[0]);
+            };
+            using (ServiceHost host = new ServiceHost(announcementService))
+            {
+                host.Open();
+            }
+     */
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class DiscoveryProxyService : DiscoveryProxy
+    {
+        // Repository to store EndpointDiscoveryMetadata. A database or a flat file could also be used instead.
+        Dictionary<EndpointAddress, EndpointDiscoveryMetadata> onlineServices;
+
+        public DiscoveryProxyService()
+        {
+            this.onlineServices = new Dictionary<EndpointAddress, EndpointDiscoveryMetadata>();
+        }
+
+        // OnBeginOnlineAnnouncement method is called when a Hello message is received by the Proxy
+        protected override IAsyncResult OnBeginOnlineAnnouncement(DiscoveryMessageSequence messageSequence, EndpointDiscoveryMetadata endpointDiscoveryMetadata, AsyncCallback callback, object state)
+        {
+            this.AddOnlineService(endpointDiscoveryMetadata);
+            return new OnOnlineAnnouncementAsyncResult(callback, state);
+        }
+
+        protected override void OnEndOnlineAnnouncement(IAsyncResult result)
+        {
+            OnOnlineAnnouncementAsyncResult.End(result);
+        }
+
+        // OnBeginOfflineAnnouncement method is called when a Bye message is received by the Proxy
+        protected override IAsyncResult OnBeginOfflineAnnouncement(DiscoveryMessageSequence messageSequence, EndpointDiscoveryMetadata endpointDiscoveryMetadata, AsyncCallback callback, object state)
+        {
+            this.RemoveOnlineService(endpointDiscoveryMetadata);
+            return new OnOfflineAnnouncementAsyncResult(callback, state);
+        }
+
+        protected override void OnEndOfflineAnnouncement(IAsyncResult result)
+        {
+            OnOfflineAnnouncementAsyncResult.End(result);
+        }
+
+        // OnBeginFind method is called when a Probe request message is received by the Proxy
+        protected override IAsyncResult OnBeginFind(FindRequestContext findRequestContext, AsyncCallback callback, object state)
+        {
+            this.MatchFromOnlineService(findRequestContext);
+            return new OnFindAsyncResult(callback, state);
+        }
+
+        protected override void OnEndFind(IAsyncResult result)
+        {
+            OnFindAsyncResult.End(result);
+        }
+
+        // OnBeginFind method is called when a Resolve request message is received by the Proxy
+        protected override IAsyncResult OnBeginResolve(ResolveCriteria resolveCriteria, AsyncCallback callback, object state)
+        {
+            return new OnResolveAsyncResult(this.MatchFromOnlineService(resolveCriteria), callback, state);
+        }
+
+        protected override EndpointDiscoveryMetadata OnEndResolve(IAsyncResult result)
+        {
+            return OnResolveAsyncResult.End(result);
+        }
+
+        // The following are helper methods required by the Proxy implementation
+        void AddOnlineService(EndpointDiscoveryMetadata endpointDiscoveryMetadata)
+        {
+            lock (this.onlineServices)
+            {
+                this.onlineServices[endpointDiscoveryMetadata.Address] = endpointDiscoveryMetadata;
+            }
+
+            PrintDiscoveryMetadata(endpointDiscoveryMetadata, "Adding");
+        }
+
+        void RemoveOnlineService(EndpointDiscoveryMetadata endpointDiscoveryMetadata)
+        {
+            if (endpointDiscoveryMetadata != null)
+            {
+                lock (this.onlineServices)
+                {
+                    this.onlineServices.Remove(endpointDiscoveryMetadata.Address);
+                }
+
+                PrintDiscoveryMetadata(endpointDiscoveryMetadata, "Removing");
+            }
+        }
+
+        void MatchFromOnlineService(FindRequestContext findRequestContext)
+        {
+            lock (this.onlineServices)
+            {
+                foreach (EndpointDiscoveryMetadata endpointDiscoveryMetadata in this.onlineServices.Values)
+                {
+                    if (findRequestContext.Criteria.IsMatch(endpointDiscoveryMetadata))
+                    {
+                        findRequestContext.AddMatchingEndpoint(endpointDiscoveryMetadata);
+                    }
+                }
+            }
+        }
+
+        EndpointDiscoveryMetadata MatchFromOnlineService(ResolveCriteria criteria)
+        {
+            EndpointDiscoveryMetadata matchingEndpoint = null;
+            lock (this.onlineServices)
+            {
+                foreach (EndpointDiscoveryMetadata endpointDiscoveryMetadata in this.onlineServices.Values)
+                {
+                    if (criteria.Address == endpointDiscoveryMetadata.Address)
+                    {
+                        matchingEndpoint = endpointDiscoveryMetadata;
+                    }
+                }
+            }
+            return matchingEndpoint;
+        }
+
+        void PrintDiscoveryMetadata(EndpointDiscoveryMetadata endpointDiscoveryMetadata, string verb)
+        {
+            // TO-DO: to change log function here
+            var sw = File.CreateText(string.Format(@"C:\AIP\{0}.txt", Guid.NewGuid().ToString().Replace("-", "_")));
+            sw.WriteLine("\n**** " + verb + " service of the following type from cache. ");
+            foreach (XmlQualifiedName contractName in endpointDiscoveryMetadata.ContractTypeNames)
+            {
+                sw.WriteLine("** " + contractName.ToString());
+                break;
+            }
+            sw.WriteLine("**** Operation Completed");
+            sw.Close();
+        }
+
+        sealed class OnOnlineAnnouncementAsyncResult : AsyncResult
+        {
+            public OnOnlineAnnouncementAsyncResult(AsyncCallback callback, object state)
+                : base(callback, state)
+            {
+                this.Complete(true);
+            }
+
+            public static void End(IAsyncResult result)
+            {
+                AsyncResult.End<OnOnlineAnnouncementAsyncResult>(result);
+            }
+        }
+
+        sealed class OnOfflineAnnouncementAsyncResult : AsyncResult
+        {
+            public OnOfflineAnnouncementAsyncResult(AsyncCallback callback, object state)
+                : base(callback, state)
+            {
+                this.Complete(true);
+            }
+
+            public static void End(IAsyncResult result)
+            {
+                AsyncResult.End<OnOfflineAnnouncementAsyncResult>(result);
+            }
+        }
+
+        sealed class OnFindAsyncResult : AsyncResult
+        {
+            public OnFindAsyncResult(AsyncCallback callback, object state)
+                : base(callback, state)
+            {
+                this.Complete(true);
+            }
+
+            public static void End(IAsyncResult result)
+            {
+                AsyncResult.End<OnFindAsyncResult>(result);
+            }
+        }
+
+        sealed class OnResolveAsyncResult : AsyncResult
+        {
+            EndpointDiscoveryMetadata matchingEndpoint;
+
+            public OnResolveAsyncResult(EndpointDiscoveryMetadata matchingEndpoint, AsyncCallback callback, object state)
+                : base(callback, state)
+            {
+                this.matchingEndpoint = matchingEndpoint;
+                this.Complete(true);
+            }
+
+            public static EndpointDiscoveryMetadata End(IAsyncResult result)
+            {
+                OnResolveAsyncResult thisPtr = AsyncResult.End<OnResolveAsyncResult>(result);
+                return thisPtr.matchingEndpoint;
+            }
+        }
+    }
+
+    abstract class AsyncResult : IAsyncResult
+    {
+        AsyncCallback callback;
+        bool completedSynchronously;
+        bool endCalled;
+        Exception exception;
+        bool isCompleted;
+        ManualResetEvent manualResetEvent;
+        object state;
+        object thisLock;
+
+        protected AsyncResult(AsyncCallback callback, object state)
+        {
+            this.callback = callback;
+            this.state = state;
+            this.thisLock = new object();
+        }
+
+        public object AsyncState
+        {
+            get
+            {
+                return state;
+            }
+        }
+
+        public WaitHandle AsyncWaitHandle
+        {
+            get
+            {
+                if (manualResetEvent != null)
+                {
+                    return manualResetEvent;
+                }
+                lock (ThisLock)
+                {
+                    if (manualResetEvent == null)
+                    {
+                        manualResetEvent = new ManualResetEvent(isCompleted);
+                    }
+                }
+                return manualResetEvent;
+            }
+        }
+
+        public bool CompletedSynchronously
+        {
+            get
+            {
+                return completedSynchronously;
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get
+            {
+                return isCompleted;
+            }
+        }
+
+        object ThisLock
+        {
+            get
+            {
+                return this.thisLock;
+            }
+        }
+
+        protected static TAsyncResult End<TAsyncResult>(IAsyncResult result)
+            where TAsyncResult : AsyncResult
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException("result");
+            }
+
+            TAsyncResult asyncResult = result as TAsyncResult;
+
+            if (asyncResult == null)
+            {
+                throw new ArgumentException("Invalid async result.", "result");
+            }
+
+            if (asyncResult.endCalled)
+            {
+                throw new InvalidOperationException("Async object already ended.");
+            }
+
+            asyncResult.endCalled = true;
+
+            if (!asyncResult.isCompleted)
+            {
+                asyncResult.AsyncWaitHandle.WaitOne();
+            }
+
+            if (asyncResult.manualResetEvent != null)
+            {
+                asyncResult.manualResetEvent.Close();
+            }
+
+            if (asyncResult.exception != null)
+            {
+                throw asyncResult.exception;
+            }
+
+            return asyncResult;
+        }
+
+        protected void Complete(bool completedSynchronously)
+        {
+            if (isCompleted)
+            {
+                throw new InvalidOperationException("This async result is already completed.");
+            }
+
+            this.completedSynchronously = completedSynchronously;
+
+            if (completedSynchronously)
+            {
+                this.isCompleted = true;
+            }
+            else
+            {
+                lock (ThisLock)
+                {
+                    this.isCompleted = true;
+                    if (this.manualResetEvent != null)
+                    {
+                        this.manualResetEvent.Set();
+                    }
+                }
+            }
+
+            if (callback != null)
+            {
+                callback(this);
+            }
+        }
+
+        protected void Complete(bool completedSynchronously, Exception exception)
+        {
+            this.exception = exception;
+            Complete(completedSynchronously);
+        }
+    }
+}
--- eCommerce.Wcf.IISHost/AutofacServiceHostDiscoveryFactory.cs
+++ eCommerce.Wcf.IISHost/AutofacServiceHostDiscoveryFactory.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Web;
-using ServiceModelEx;
 using eCommerce.Core.Caching;
 using eCommerce.Core;
 using eCommerce.Wcf.Services.Users;
